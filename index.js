00fa480000844369d93d6032bf118de86c3180d8
jakedavies <jgdavies24@gmail.com>
2016-03-09 11:54:55 -0800

    restructured to be more nodey

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-module.exports('./src/fb');
+"use strict";
+
+var FB = function(options){
+  var api = {};
+
+  api.accounts = require('./src/accounts')(options);
+  api.user = require('./src/user')(options);
+  api.ads = require('./src/ads')(options);
+  api.creatives = require('./src/creatives')(options);
+  api.campaigns = require('./src/campaigns')(options);
+
+  return api;
+};
+
+/*
+Export the module
+ */
+exports = module.exports = FB;
